cdbe0b1d991810e46ddce9e6a67e00037294e5b4
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2022-06-04 09:56:40 +0500

    44442

--- 7.c
+++ 7.c
@@ -1,110 +1,86 @@
-// Изменить порядок строк в матрице таким образом, чтобы элементы ПЕРВОГО
-// столбца были расположены в порядке убывания.
+//Здесь без функций
 #include <conio.h>
+#include <malloc.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+//Заполнение
+// void fill(int r, int c) {
 //
-// void sort(int **a, int c, int r) { //Все правильно
-//   int j, t;
-//   for (int i = 0; i < r; i++)
-//     for (int j = 0; j < r; j++)
-//       if (a[i][0] > a[j][0])
-//         for (int k = 0; k < r; k++) {
-//           int t = a[i][0];
-//           a[i][0] = a[j][0];
-//           a[j][0] = t;
-//         }
-// }
-//
-// void fill(int n, int d, int **a) {
-//   for (int i = 0; i < n; i++) {
-//     for (int j = 0; j < d; j++) {
-//       a[i][j] = rand() % 10 - 5;
-//     }
-//   }
-//   for (int i = 0; i < 6; i++) {
-//     for (int j = 0; j < 6; j++) {
-//       printf("%4d", a[i][j]);
-//       if (j == 6 - 1)
-//         printf("\n");
-//     }
-//   }
-// }
-//
-// void view(int a, int r, int c) {
+//   srand(time(NULL));
 //   for (int i = 0; i < r; i++)
 //     for (int j = 0; j < c; j++) {
-//       printf("%4d", a[i][j]);
-//       if (j == c - 1)
-//         printf("\n");
+//       // *(a + r * c + j)= rand() % 10 - 1;
+//       // Найди как правльно указать на элемент
+//       *(a + (r * c) + (i + j)) = rand() % 10 - 0;
 //     }
+//   printf("matr v fill\n");
+//   for (int i = 0; i < r; i++) {
+//     for (int j = 0; j < c; j++)
+//       //  printf("%4d", *(a + r * c + i) );
+//       printf("%4d", *(a + r * c + i));
+//
+//     printf("\n");
+//   }
+//   printf("\n");
+//   printf("\n");
+//   printf("\n");
+//   printf("\n");
+//
+//   // a - указатель на массив,
+//   // r - количество столбцов,
+//   // j - индекс строки,
+//   // i - индекс столбца.
 // }
+//Вывод
+// void output(int *a, int r, int c)
+void output(int r, int c, int a[r][c]) {
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      // printf( "%4d", (int*)a++ );
+      // printf("%4d ", *a);
+      // printf("%4d ", *(a + i + j));
+      // printf("%4d ", (*(a+i))[j]);
+      printf("%4d ", a[i][j]);
+    }
+    printf("\n");
+  }
+}
+//Сортировка
+// void sort(int *a, int r, int c)
+void sort(int r, int c, int a[r][c]) {
+  int t;
+  for (int i = 0; i < r; i++)
+    for (int j = 0; j < r; j++)
+      if (a[i][0] > a[j][0]) {
+        // if ((a[i][0] || a[j][0]) > 100)
+        //  printf("%d --- %d\n", i, j);
+        for (int k = 0; k < r; k++) {
+          int t = a[i][k];
+          a[i][k] = a[j][k];
+          a[j][k] = t;
+        }
+      }
+}
 
 int main() {
-    srand(time(NULL));
-  int x, d, i, j, n, r, c, noe;
-  int columns, rows;
-  printf("columns --->");
-  scanf("%d", &columns);
-  printf("rows --->");
-  scanf("%d", &rows);
-  int a[columns][rows];
+  srand(time(NULL));
+  int r, c;
+  printf("rows -> ");
+  scanf("%d", &r);
+  printf("columns -> ");
+  scanf("%d", &c);
+  int a[r][c];
 
-  printf("rows: %d\n", sizeof(a) / sizeof(a[0]));
-  printf("columns: %d\n", sizeof(a[0]) / sizeof(a[0][0]));
-  printf("number of elements: %d\n", sizeof(a) / sizeof(a[0][0]));
-  r = sizeof(a) / sizeof(a[0]);
-  c = sizeof(a[0]) / sizeof(a[0][0]);
-  noe = sizeof(a) / sizeof(a[0][0]);
+  for (int i = 0; i < r; i++)
+    for (int j = 0; j < c; j++)
+      a[i][j] = rand() % 10 - 0;
 
-  // fill
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < d; j++)
-      a[i][j] = rand() % 10 - 5;
-
-    printf("matr\n");
-    // view
-    for (int i = 0; i < r; i++)
-      for (int j = 0; j < c; j++) {
-        printf("%4d", a[i][j]);
-        if (j == c - 1)
-          printf("\n");
-      }
-
-    // sort
-    for (int i = 0; i < r; i++)
-      for (int j = 0; j < r; j++)
-        if (a[i][0] > a[j][0])
-          for (int k = 0; k < r; k++) {
-            int t = a[i][0];
-            a[i][0] = a[j][0];
-            a[j][0] = t;
-          }
-    printf("new matr\n");
-    // new view
-    for (int i = 0; i < r; i++)
-      for (int j = 0; j < c; j++) {
-        printf("%4d", a[i][j]);
-        if (j == c - 1)
-          printf("\n");
-      }
-    //  view(a,5,4);
-    // for (i = 0; i < r; i++)
-    //   for (j = 0; j < c; j++) {
-    //     printf("%4d", a[i][j]);
-    //     if (j == c - 1)
-    //       printf("\n");
-    //   }
-    // for (int i = 0; i < r; i++)
-    //   for (int j = 0; j < r; j++)
-    //     if (a[i][0] > a[j][0])
-    //       for (int k = 0; k < r; k++) {
-    //         int t = a[i][0];
-    //         a[i][0] = a[j][0];
-    //         a[j][0] = t;
-    //       }
-
-    return 0;
-  }
+  printf("Matrix before sorting:\n");
+  output(r, c, a);
+  sort(r, c, a);
+  printf("Matrix after sorting:\n");
+  output(r, c, a);
+}
